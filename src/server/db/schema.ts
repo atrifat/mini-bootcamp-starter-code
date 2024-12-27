8d4b07c70040fb50ed985d2d1b5b3388eec3e7d6
Rif'at Ahdi R <10791791+atrifat@users.noreply.github.com>
2024-12-27 09:43:16 +0800

    Prettier

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -14,7 +14,9 @@ import { type AdapterAccount } from "next-auth/adapters";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = sqliteTableCreator((name) => `audiopintar.com_${name}`);
+export const createTable = sqliteTableCreator(
+  (name) => `audiopintar.com_${name}`,
+);
 
 export const users = createTable("user", {
   id: text("id", { length: 255 })
@@ -57,7 +59,7 @@ export const accounts = createTable(
       columns: [account.provider, account.providerAccountId],
     }),
     userIdIdx: index("account_user_id_idx").on(account.userId),
-  })
+  }),
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -75,7 +77,7 @@ export const sessions = createTable(
   },
   (session) => ({
     userIdIdx: index("session_userId_idx").on(session.userId),
-  })
+  }),
 );
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
@@ -91,5 +93,5 @@ export const verificationTokens = createTable(
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+  }),
 );
