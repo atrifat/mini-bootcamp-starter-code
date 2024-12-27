8de005d315baddd0b5928a7ae7b26ad533aaf868
Rif'at Ahdi R <10791791+atrifat@users.noreply.github.com>
2024-12-27 09:45:48 +0800

    wip: show basic user profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,6 +51,20 @@ export default async function Home() {
             {session && (
               <div className="mt-8 rounded-2xl border border-purple-500/20 bg-white/5 px-8 py-16 backdrop-blur-sm">
                 {/* Document list will be here */}
+                <div className="bg-white-800 flex items-center space-x-4 rounded-lg p-4">
+                  <div className="relative h-24 w-24 overflow-hidden rounded-full">
+                    <img
+                      src={session?.user.image ?? "person_placeholder.png"}
+                      alt={session?.user.name ?? ""}
+                      className="h-full w-full object-cover"
+                    />
+                  </div>
+                  <div>
+                    <h2 className="text-center text-xl font-semibold text-white">
+                      {session?.user.name ?? ""}
+                    </h2>
+                  </div>
+                </div>
               </div>
             )}
 
